f0188d24cd305e81a9a94fc17531a41b137ff3f8
hyos <hyozzang99@gmail.com>
2025-11-09 19:22:36 +0900

    Feat: JWT 생성 및 검증 기능 구현
          - createToken(): userId, email, 권한 정보를 담아 JWT 생성, 발급일 및 만료일 설정
          - substringToken(): Authorization 헤더에서 'Bearer ' 접두사 제거
          - extractClaims(): JWT를 파싱하여 Claims 추출, 인증에 활용
          - HS256 알고리즘과 Base64 디코딩된 secretKey 사용

---
+++ src/main/java/hyos1/myapp/config/JwtUtil.java
@@ -0,0 +1,68 @@
+package hyos1.myapp.config;
+
+import hyos1.myapp.common.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+public class JwtUtil {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final long TOKEN_TIME = 30 * 1 * 1000L; //30초
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    //토큰 생성
+    public String createToken(Long userId, String email, UserRole userRole) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(userId))
+                        .claim("email", email)
+                        .claim("userRole", userRole.getUserRole())
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) //만료일 설정
+                        .setIssuedAt(date) //발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
+    }
+
+    //토큰에서 Bearer 제거
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        log.error("Not Found Token");
+        throw new NullPointerException("Not Found Token");
+    }
+
+    //Claims 추출
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
