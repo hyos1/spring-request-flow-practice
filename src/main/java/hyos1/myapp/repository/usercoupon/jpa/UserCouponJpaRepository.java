2be9f09cfac7ae0f164a3b0c15a226863b460a51
hyos <hyozzang99@gmail.com>
2025-11-12 16:09:54 +0900

    Feat: UserCouponRepository 구현 및 단건 조회, 전체 조회, 중복 발급 확인 기능 추가

--- src/main/java/hyos1/myapp/repository/usercoupon/jpa/UserCouponJpaRepository.java
+++ src/main/java/hyos1/myapp/repository/usercoupon/jpa/UserCouponJpaRepository.java
@@ -2,6 +2,7 @@ package hyos1.myapp.repository.usercoupon.jpa;
 
 import hyos1.myapp.entity.UserCoupon;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -28,13 +29,38 @@ public class UserCouponJpaRepository implements UserCouponRepository {
         return Optional.ofNullable(userCoupon);
     }
 
-    //[관리자]전체 사용자 쿠폰 목록 조회
+    // 사용자가 발급받은 쿠폰 단건 조회
     @Override
-    public List<UserCoupon> findAll() {
-        return em.createQuery("select uc from UserCoupon", UserCoupon.class)
+    public Optional<UserCoupon> findByUserIdAndCouponId(Long userId, Long couponId) {
+        try {
+            UserCoupon userCoupon = em.createQuery("select uc from UserCoupon uc " +
+                            "where uc.user.id = :userId " +
+                            "and uc.coupon.id = :couponId", UserCoupon.class)
+                    .setParameter("userId", userId)
+                    .setParameter("couponId", couponId)
+                    .getSingleResult();
+            return Optional.of(userCoupon);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    // 사용자가 발급받은 쿠폰 전체 조회
+    @Override
+    public List<UserCoupon> findAllByUserId(Long userId) {
+        return em.createQuery("select uc from UserCoupon uc where uc.user.id = :userId", UserCoupon.class)
+                .setParameter("userId", userId)
                 .getResultList();
     }
 
+    //[관리자]전체 사용자 쿠폰 목록 조회
+    @Override
+    public List<UserCoupon> findAll() {
+        return em.createQuery("select uc from UserCoupon uc", UserCoupon.class)
+                .getResultList();
+    }
+
+    // 사용자가 해당 쿠폰 발급 받았었는지 확인
     @Override
     public boolean existsByUserIdAndCouponId(Long userId, Long couponId) {
         Long count = em.createQuery("select count(uc) from UserCoupon uc " +
--- src/main/java/hyos1/myapp/repository/usercoupon/jpa/UserCouponRepository.java
+++ src/main/java/hyos1/myapp/repository/usercoupon/jpa/UserCouponRepository.java
@@ -7,11 +7,21 @@ import java.util.Optional;
 
 public interface UserCouponRepository {
 
+    // 유저가 쿠폰 발급
     UserCoupon save(UserCoupon userCoupon);
 
+    // 유저가 발급받은 쿠폰 단건 조회
+    Optional<UserCoupon> findByUserIdAndCouponId(Long userId, Long couponId);
+
+    // 유저가 발급받은 쿠폰 전체 조회
+    List<UserCoupon> findAllByUserId(Long userId);
+
+    // [관리자]UserCoupon 단건 조회
     Optional<UserCoupon> findById(Long userCouponId);
+
+    // [관리자]모든 회원이 발급받은 쿠폰 전체 조회
     List<UserCoupon> findAll();
 
+    //발급받은 적 있는지 확인
     boolean existsByUserIdAndCouponId(Long userId, Long couponId);
-
 }
