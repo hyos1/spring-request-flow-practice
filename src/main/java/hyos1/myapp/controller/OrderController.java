00ab8c91d4beb6db3fbffd5e2fa29b65c4fbbe76
hyos <hyozzang99@gmail.com>
2025-11-18 00:04:51 +0900

    Feat: OrderController 구현 - 사용자: 주문 생성, 단건 조회, 전체 조회, 취소 - 관리자: 주문 단건 조회, 전체 조회

--- src/main/java/hyos1/myapp/controller/OrderController.java
+++ src/main/java/hyos1/myapp/controller/OrderController.java
@@ -8,8 +8,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -25,5 +28,46 @@ public class OrderController {
             @Valid @RequestBody OrderCreateRequest request) {
         OrderResponse response = orderService.createOrder(authUser.getUserId(), request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // [사용자] 본인 주문 단건 조회 ✓
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getMyOrder(
+            @PathVariable Long orderId,
+            @AuthenticationPrincipal AuthUser authUser) {
+        OrderResponse response = orderService.findUserOrderById(authUser.getUserId(), orderId);
+        return ResponseEntity.ok(response);
+    }
+
+    // [사용자] 본인 주문 전체 조회
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getMyOrders(@AuthenticationPrincipal AuthUser authUser) {
+        List<OrderResponse> userOrders = orderService.findUserOrders(authUser.getUserId());
+        return ResponseEntity.ok(userOrders);
+    }
+
+    // [사용자] 주문 취소
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> cancelOrder(
+            @PathVariable Long orderId,
+            @AuthenticationPrincipal AuthUser authUser) {
+        orderService.cancelOrder(authUser.getUserId(), orderId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // [관리자] 주문 단건 조회
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin/{orderId}")
+    public ResponseEntity<OrderResponse> findByIdAdmin(@PathVariable Long orderId) {
+        OrderResponse response = orderService.findOrderById(orderId);
+        return ResponseEntity.ok(response);
+    }
+
+    // [관리자] 주문 전체 조회
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<List<OrderResponse>> findAllAdmin() {
+        List<OrderResponse> orders = orderService.findAllOrders();
+        return ResponseEntity.ok(orders);
     }
 }
