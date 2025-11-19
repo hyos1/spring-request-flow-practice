2a6eae539e917736731a41778781f894cb5817dc
hyos <hyozzang99@gmail.com>
2025-11-19 20:37:52 +0900

    Feat: 예외 발생 시 클라이언트에 보낼 응답 정의

---
+++ src/main/java/hyos1/myapp/common/exception/constant/ErrorResponse.java
@@ -0,0 +1,23 @@
+package hyos1.myapp.common.exception.constant;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String errorName;
+    private int status;
+    private String message;
+
+    public ErrorResponse(String errorName, int status, String message) {
+        this.errorName = errorName;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ErrorResponse from(ErrorCode code) {
+        return new ErrorResponse(code.name(), code.getHttpStatus().value(), code.getMessage());
+    }
+}
