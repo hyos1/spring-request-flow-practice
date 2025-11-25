499989ece2c7f062043056f430517797668dffb7
hyos <hyozzang99@gmail.com>
2025-11-26 01:23:15 +0900

    Test: UserController 단위 테스트 추가 - 일반 사용자가 관리자 권한 기능 호출 시 Forbidden 테스트 추가 - updateUser API 성공 테스트 구현 - MockMvc 인증(authentication()) 기반 Security 테스트

--- src/test/java/hyos1/myapp/controller/UserControllerTest.java
+++ src/test/java/hyos1/myapp/controller/UserControllerTest.java
@@ -1,9 +1,12 @@
 package hyos1.myapp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hyos1.myapp.common.UserRole;
+import hyos1.myapp.config.CustomAuthenticationEntryPoint;
 import hyos1.myapp.config.JwtAuthenticationToken;
 import hyos1.myapp.config.JwtUtil;
 import hyos1.myapp.config.SecurityConfig;
+import hyos1.myapp.dto.request.UserUpdateRequest;
 import hyos1.myapp.dto.response.UserResponse;
 import hyos1.myapp.entity.AuthUser;
 import hyos1.myapp.service.UserService;
@@ -20,13 +23,14 @@ import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@AutoConfigureMockMvc(addFilters = false) // security 필터 제거
 
 @WebMvcTest(UserController.class)
-@Import({SecurityConfig.class, JwtUtil.class})
+@Import({SecurityConfig.class, JwtUtil.class, CustomAuthenticationEntryPoint.class})
 class UserControllerTest {
 
     @MockitoBean
@@ -34,6 +38,8 @@ class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private JwtAuthenticationToken adminAuthenticationToken;
     private JwtAuthenticationToken userAuthenticationToken;
@@ -76,8 +82,29 @@ class UserControllerTest {
         ));
         BDDMockito.when(userService.findAll()).thenReturn(list);
 
-        mockMvc.perform(get("/users").with(authentication(userAuthenticationToken)))
+        mockMvc.perform(get("/users").with(authentication(adminAuthenticationToken)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].email").value(user.getEmail()));
+    }
+
+    @Test
+    void findAll_fail() throws Exception {
+        mockMvc.perform(get("/users").with(authentication(userAuthenticationToken)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void updateUser() throws Exception {
+        UserUpdateRequest request = new UserUpdateRequest("newEmail@naver.com", "1234", "12345");
+
+        UserResponse response = new UserResponse(user.getUserId(), "기존이름", request.getEmail(), UserRole.ROLE_USER);
+        BDDMockito.when(userService.updateUser(user.getUserId(), user.getUserId(), request))
+                .thenReturn(response);
+
+        mockMvc.perform(patch("/users/{userId}", user.getUserId())
+                        .with(authentication(userAuthenticationToken))
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
     }
 }
