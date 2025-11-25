d2815cacf743eab38f1bc3e10a50c6d2d9dae433
hyos <hyozzang99@gmail.com>
2025-11-25 23:07:30 +0900

    Test: UserController 단위 테스트 추가 - /users/me API 정상 조회 테스트 - /users API 전체 조회 테스트 - 인증된 사용자 기반 MockMvc 요청 테스트

--- src/main/java/hyos1/myapp/dto/request/UserUpdateRequest.java
+++ src/main/java/hyos1/myapp/dto/request/UserUpdateRequest.java
@@ -3,9 +3,13 @@ package hyos1.myapp.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateRequest {
 
     @Email
---
+++ src/test/java/hyos1/myapp/controller/UserControllerTest.java
@@ -0,0 +1,83 @@
+package hyos1.myapp.controller;
+
+import hyos1.myapp.common.UserRole;
+import hyos1.myapp.config.JwtAuthenticationToken;
+import hyos1.myapp.config.JwtUtil;
+import hyos1.myapp.config.SecurityConfig;
+import hyos1.myapp.dto.response.UserResponse;
+import hyos1.myapp.entity.AuthUser;
+import hyos1.myapp.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@AutoConfigureMockMvc(addFilters = false) // security 필터 제거
+
+@WebMvcTest(UserController.class)
+@Import({SecurityConfig.class, JwtUtil.class})
+class UserControllerTest {
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private JwtAuthenticationToken adminAuthenticationToken;
+    private JwtAuthenticationToken userAuthenticationToken;
+
+    private AuthUser admin;
+    private AuthUser user;
+
+    @BeforeEach
+    void setUp() {
+        admin = new AuthUser(1L, "admin@example.com", UserRole.ROLE_ADMIN);
+        adminAuthenticationToken = new JwtAuthenticationToken(admin);
+
+        user = new AuthUser(2L, "user@example.com", UserRole.ROLE_USER);
+        userAuthenticationToken = new JwtAuthenticationToken(user);
+    }
+
+    @Test
+    void findMe_success() throws Exception {
+        // given
+
+        UserResponse response = new UserResponse(
+                user.getUserId(), "홍길동", user.getEmail(), UserRole.ROLE_USER
+        );
+
+        BDDMockito.when(userService.findById(user.getUserId()))
+                .thenReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/users/me")
+                        .with(authentication(userAuthenticationToken)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(user.getUserId()))
+                .andExpect(jsonPath("$.email").value(user.getEmail()));
+    }
+
+    @Test
+    void findAll_success() throws Exception {
+        List<UserResponse> list = List.of(new UserResponse(
+                user.getUserId(), "홍길동", user.getEmail(), UserRole.ROLE_USER
+        ));
+        BDDMockito.when(userService.findAll()).thenReturn(list);
+
+        mockMvc.perform(get("/users").with(authentication(userAuthenticationToken)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].email").value(user.getEmail()));
+    }
+}
